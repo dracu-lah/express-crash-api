611013815621633e5e311fa278f798ffa8489c1b
dracu-lah <nevilnicks4321@gmail.com>
2024-07-15 15:54:15 +0530

    added swagger

--- server.js
+++ server.js
@@ -1,26 +1,41 @@
 import express from "express";
-// import path from "path";
-// import { fileURLToPath } from "url";
+import swaggerUi from "swagger-ui-express";
+import { readFile } from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
 // Get the __dirname
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const port = process.env.PORT || 8000;
 const app = express();
-const port = process.env.PORT | 8000;
+
+const filePath = path.join(__dirname, "./swagger-output.json");
+const swaggerDocument = JSON.parse(await readFile(filePath, "utf8"));
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 import posts from "./routes/posts.js";
 import errorHandler from "./middleware/error.js";
 import logger from "./middleware/logger.js";
 import notFound from "./middleware/notFound.js";
-// setup static folder
-// app.use(express.static(path.join(__dirname, "public")));
+
+// Setup static folder
+app.use(express.static(path.join(__dirname, "public")));
+
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 // Logger Middleware
 app.use(logger);
+
 // Routes
 app.use("/api/posts", posts);
 
 // Error Handler Middleware
 app.use(notFound);
 app.use(errorHandler);
-app.listen(port, () => console.log("Server is running at " + port));
+
+app.listen(port, () => console.log(`Server is running at port ${port}`));
